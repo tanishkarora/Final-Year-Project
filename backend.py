a1e9a74a2d1626930c998db26143d0fc1498fbe8
tanishkarora <66875408+tanishkarora@users.noreply.github.com>
2023-05-06 16:29:45 +0530

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,8 +1,8 @@
 import pickle
 from flask import Flask, request, jsonify
 import numpy as np
+import pandas as pd
 from flask_cors import CORS
-
 
 
 app = Flask(__name__)
@@ -36,15 +36,27 @@ def submit_values():
     value12 = "No"
     value14 = "Low"
 
+
+    cols = ['Gender', 'Age', 'Education Level', 'Institution Type', 'IT Student', 'Location', 'Load-shedding', 'Financial Condition', 'Internet Type', 'Network Type', 'Class Duration', 'Self Lms', 'Device', 'Adaptivity Level']
+    data = [[gender, age, educationLevel, institutionType, itStudent, location, value7, financialCondition, internetType, networkType, classDuration, value12, device, value14]]
+
+    df = pd.DataFrame(data, columns=cols)
+
     # Use the loaded encoder to transform the input values
-    input_values = [[gender, age, educationLevel, institutionType, itStudent, location, value7, financialCondition, internetType, networkType, classDuration, value12, device ,value14]]
-    input_values_encoded = encoder.transform(input_values)
+    
+    input_values_encoded = encoder.transform(df)
 
-    input_values_encoded = np.delete(input_values_encoded, [6, 11, 13], axis=1)
+    # converting encoded values to pandas dataframe
 
+    encoded_df = pd.DataFrame(input_values_encoded, columns=cols)
+   
+    # dropping the values not used for prediction but had to be used in encoder 
 
+    encoded_df_final = encoded_df.drop(['Load-shedding', 'Self Lms', 'Adaptivity Level'], axis=1)
+    
     # Perform prediction using the loaded model with the encoded input values
-    prediction = model.predict(input_values_encoded)
+    
+    prediction = model.predict(encoded_df_final)
 
     # You can further process the prediction, e.g. convert it to JSON or return it in a specific format
     # In this example, we simply return the prediction as a string
